adb348f3b50954e97bfbddc44bf654c05fc496c7
Jeroen Woudstra <j.woudstra@ovistelematics.com>
2017-10-13 11:24:45 +0200

    Changes after review

--- Resources/public/js/leaflet-embed.js
+++ Resources/public/js/leaflet-embed.js
@@ -15,34 +15,19 @@ var CuriousMap = function(options) {
   this.zoom = options.zoom;
   this.mapId = options.mapId;
 
-  // Defined input ids
-  var idPrefix = options.fieldId.id;
-  this.currentLocationId = '#' + idPrefix + '_location_snap';
-  this.searchAddressInputId = '#' + idPrefix + '_location_search_input';
-  this.searchButtonId = '#' + idPrefix + '_location_search_btn';
-  $(this.searchAddressInputId).focus();
+  // Configured elements
+  var formIdPrefix = options.fieldId;
+  this.initialiseFormFields(options.fields, formIdPrefix);
 
-  // Configured input ids based on allowed fields in MapType class
-  this.inputIds = {};
-  this.inputIds.latitude = getFieldId(options, 'latitude', idPrefix);
-  this.inputIds.longitude = getFieldId(options, 'longitude', idPrefix);
-  this.inputIds.address = getFieldId(options, 'address', idPrefix);
-  this.inputIds.street = getFieldId(options, 'street', idPrefix);
-  this.inputIds.postal_code = getFieldId(options, 'postal_code', idPrefix);
-  this.inputIds.city = getFieldId(options, 'city', idPrefix);
-  this.inputIds.city_district = getFieldId(options, 'city_district', idPrefix);
-  this.inputIds.city_neighbourhood = getFieldId(options, 'city_neighbourhood', idPrefix);
-  this.inputIds.state = getFieldId(options, 'state', idPrefix);
-  this.inputIds.country = getFieldId(options, 'country', idPrefix);
+  // Templated element ids
+  this.snapButtonId = '#' + formIdPrefix + '_location_snap';
+  this.searchAddressInputId = '#' + formIdPrefix + '_location_search_input';
+  this.searchButtonId = '#' + formIdPrefix + '_location_search_btn';
+  $(this.searchAddressInputId).focus();
 
   // Set up the map
   this.map = new L.Map(this.mapId);
-  this.map.on('locationfound', function(e) {
-    var position = e.latlng;
-    this.updatePosition(position);
-    this.updateMap(position);
-    this.setMarkerFormFields();
-  });
+  this.map.on('locationfound', this.mapLocationFound);
 
   // Create the tile layer with correct attribution
   var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -54,21 +39,15 @@ var CuriousMap = function(options) {
   this.map.addLayer(osm);
 
   // Add marker
-  this.marker = this.addMarker();
-
-  // Set form fields
-  this.setMarkerFormFields();
+  this.marker = L.marker([this.lat, this.long], {draggable: 'true'}).addTo(this.map);
   this.marker.on('dragend', function() {
-    var position = $this.marker.getLatLng();
-    $this.updatePosition(position);
-    $this.updateMap(position);
-    populateInputs(position);
+    $this.updateMap(this.getLatLng());
   });
 
   // Have the enter key submit the search instead of the form
-  $(document).on('keydown', this.searchAddressInputId, function(event) {
-    if (event.keyCode === 13) {
-      event.preventDefault();
+  $(document).on('keydown', this.searchAddressInputId, function(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
       $($this.searchButtonId).click();
       return false;
     }
@@ -86,99 +65,130 @@ var CuriousMap = function(options) {
       if (data.length !== 0) {
         var details = data[0];
         var position = new L.LatLng(details.lat, details.lon);
-        $this.updatePosition(position);
         $this.updateMap(position);
-        populateInputs(position);
 
         // Zoom to appropriate level
-        $this.map.setZoom(zoomByType(details.type));
+        $this.map.setZoom($this.determineZoomLevel(details.type));
       }
     });
   });
-
-  // Populate the configured fields
-  function populateInputs(position) {
-    $.getJSON('https://nominatim.openstreetmap.org/reverse?lat=' + position.lat + '&lon=' + position.lng + '&zoom=18&addressdetails=1&limit=1&format=json', function(data) {
-      // Update the values that are always present
-      $($this.inputIds.latitude).val(position.lat);
-      $($this.inputIds.longitude).val(position.lng);
-
-      // Update additional address data
-      if (data.length !== 0 && typeof data.address !== 'undefined') {
-
-        // Determine city district information
-        var district = data.address.suburb;
-        if (data.address.suburb === data.address.city && typeof data.address.residential !== 'undefined') {
-          district = data.address.residential;
-        } else if (data.address.suburb === data.address.city && typeof data.address.industrial !== 'undefined') {
-          district = data.address.industrial;
-        }
-
-        // Populate the actual fields
-        $($this.inputIds.address).val(data.display_name);
-        $($this.inputIds.street).val(data.address.road);
-        $($this.inputIds.postal_code).val(data.address.postcode);
-        $($this.inputIds.city).val(data.address.city);
-        $($this.inputIds.city_district).val(district);
-        $($this.inputIds.city_neighbourhood).val(data.address.neighbourhood);
-        $($this.inputIds.state).val(data.address.state);
-        $($this.inputIds.country).val(data.address.country);
-      }
-    });
-  }
-
-  // Generate dynamically configured input field ids
-  function getFieldId(options, requestedFieldName, idPrefix) {
-    var fieldId;
-    if (typeof options.fields[requestedFieldName] !== 'undefined') {
-      if (typeof options.fields[requestedFieldName].name !== 'undefined') {
-        fieldId = '#' + idPrefix + '_' + options.fields[requestedFieldName].name;
-      } else {
-        fieldId = '#' + idPrefix + '_' + requestedFieldName;
-      }
-    }
-    return fieldId;
-  }
-
-  // Determine appropriate zoomlevel for our map
-  function zoomByType(type) {
-    var zoom;
-    if (type === 'house') {
-      zoom = 18;
-    } else if (type === 'neighbourhood') {
-      zoom = 16
-    } else if (type === 'city') {
-      zoom = 13
-    } else if (type === 'administrative') {
-      zoom = 11
-    } else {
-      zoom = 8
-    }
-    return zoom;
-  }
-
 };
 
-// Create a marker to add to our map
-CuriousMap.prototype.addMarker = function() {
-  return L.marker([this.lat, this.long], {draggable: 'true'}).addTo(this.map);
-};
-
-// Update this model's position information
-CuriousMap.prototype.updatePosition = function(position) {
+/**
+ * Update the marker and map location
+ *
+ * @param position
+ */
+CuriousMap.prototype.updateMap = function(position) {
   this.lat = position.lat;
   this.long = position.lng;
-};
-
-// Update the marker and map location
-CuriousMap.prototype.updateMap = function(position) {
   this.marker.setLatLng(position, {draggable: 'true'});
   this.map.panTo(position);
+  this.updateFormFields(position);
 };
 
-// Populate the lat and long fields (defaults)
-CuriousMap.prototype.setMarkerFormFields = function() {
-  var position = this.marker.getLatLng();
-  $(this.inputIds.latitude).val(position.lat);
-  $(this.inputIds.longitude).val(position.lng);
+/**
+ * Perform a reverse lookup for the provided position
+ * in order to retrieve address information and update
+ * the related form fields.
+ *
+ * @param position
+ */
+CuriousMap.prototype.updateFormFields = function(position) {
+  var $this = this;
+
+  $.getJSON('https://nominatim.openstreetmap.org/reverse?lat=' + position.lat + '&lon=' + position.lng + '&zoom=18&addressdetails=1&limit=1&format=json', function(data) {
+    // Update the values that are always present
+    $this.fields.latitude.$field.val(position.lat);
+    $this.fields.longitude.$field.val(position.lng);
+
+    // Update additional address data
+    if (data.length !== 0 && typeof data.address !== 'undefined') {
+
+      // Populate the actual fields
+      if ($this.fields.address !== undefined) {
+        $this.fields.address.$field.val(data.display_name);
+      }
+      if ($this.fields.street !== undefined) {
+        $this.fields.street.$field.val(data.address.road);
+      }
+      if ($this.fields.postal_code !== undefined) {
+        $this.fields.postal_code.$field.val(data.address.postcode);
+      }
+      if ($this.fields.city !== undefined) {
+        var city = data.address.city;
+        if (typeof data.address.town !== 'undefined') {
+          city = data.address.town;
+        } else if (typeof data.address.village !== 'undefined') {
+          city = data.address.village;
+        }
+        $this.fields.city.$field.val(city);
+      }
+      if ($this.fields.city_district !== undefined) {
+        var district = data.address.suburb;
+        if (typeof data.address.residential !== 'undefined') {
+          district = data.address.residential;
+        } else if (typeof data.address.industrial !== 'undefined') {
+          district = data.address.industrial;
+        }
+        $this.fields.city_district.$field.val(district);
+      }
+      if ($this.fields.city_neighbourhood !== undefined) {
+        $this.fields.city_neighbourhood.$field.val(data.address.neighbourhood);
+      }
+      if ($this.fields.state !== undefined) {
+        $this.fields.state.$field.val(data.address.state);
+      }
+      if ($this.fields.country !== undefined) {
+        $this.fields.country.$field.val(data.address.country);
+      }
+    }
+  });
+};
+
+/**
+ * Format the form fields array to contain
+ * - our internal input field name as key
+ * - optionally defined field name
+ * - jquery field selector
+ *
+ * @param fields
+ * @param formIdPrefix
+ */
+CuriousMap.prototype.initialiseFormFields = function(fields, formIdPrefix) {
+  var $this = this;
+
+  $this.fields = {};
+  $.each(fields, function(name, options) {
+    if (options.name === undefined) {
+      options.name = name;
+    }
+
+    options.$field = $(`#${formIdPrefix}_${options.name}`);
+    $this.fields[name] = options;
+  });
+};
+
+/**
+ * Determine appropriate zoomlevel based on information type
+ *
+ * @param type
+ * @returns int
+ */
+CuriousMap.prototype.determineZoomLevel = function(type) {
+  console.log(type);
+
+  var level;
+  if (type === 'house' || type === 'residential') {
+    level = 18;
+  } else if (type === 'neighbourhood') {
+    level = 16
+  } else if (type === 'city') {
+    level = 13
+  } else if (type === 'administrative') {
+    level = 11
+  } else {
+    level = 8
+  }
+  return level;
 };
