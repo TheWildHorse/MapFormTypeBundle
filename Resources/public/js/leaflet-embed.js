dbbe88c85b13bb236b026a246a1d5603061e8b38
webber <webber.nl@gmail.com>
2017-10-18 14:30:02 +0200

    Added possibility to configure baseLayers, overlays. Added layer UI. Added Snap to geometry in layer

--- Resources/public/js/leaflet-embed.js
+++ Resources/public/js/leaflet-embed.js
@@ -10,46 +10,128 @@ var CuriousMap = function (options) {
   this.formId = options.formId;
   this.mapId = this.formId + '_map';
   this.$modal = $(`#${this.formId}_modal`);
+  this.$mapControl = L.control.layers();
+  this.geoJsonLayerObjects = [];
 
   // Set defaults
   this.lat = options.defaults.latitude;
   this.long = options.defaults.longitude;
   this.zoom = options.defaults.zoom;
 
-  // Initialise Map
-  this.map = new L.Map(this.mapId);
-  this.initialiseBaseLayer(options.baseLayer);
-  this.initialiseOverlays(options.overlays);
+  // Initialise
+  this.initialise(options);
+};
 
-  this.map.setView(new L.LatLng(this.lat, this.long), this.zoom);
-  this.updateMarker();
+/*
+ * Initialise CuriousMap
+ */
+CuriousMap.prototype.initialise = function (options) {
+  // Initiate the Leaflet map
+  this.$map = new L.Map(this.mapId);
 
-  // Initialise CuriousMap
+  // Initialise CuriousMap FormType
   this.initialiseSearchFields();
   this.initialiseFormFields(options.fields);
+  this.initialiseMapElements(options)
   this.initialiseTriggers();
   this.focus();
 };
 
 /**
+ * Initialise search fields
+ */
+CuriousMap.prototype.initialiseSearchFields = function () {
+  this.$snapCurrent = $(`#${this.formId}_location_snap`);
+  this.$searchField = $(`#${this.formId}_location_search_input`);
+  this.$searchBtn = $(`#${this.formId}_location_search_btn`);
+};
+
+/**
+ * Link the form fields to this object
+ */
+CuriousMap.prototype.initialiseFormFields = function (fields) {
+  var $this = this;
+
+  $this.fields = {};
+  $.each(fields, function (name, options) {
+    if (options.name === undefined) {
+      options.name = name;
+    }
+
+    options.$field = $(`#${$this.formId}_${options.name}`);
+    $this.fields[name] = options;
+  });
+};
+
+/*
+ * Generate map elements
+ */
+CuriousMap.prototype.initialiseMapElements = function (options) {
+  // Add Map Control to Map
+  this.$mapControl.addTo(this.$map);
+
+  // Set view
+  this.$map.setView(new L.LatLng(this.lat, this.long), this.zoom);
+
+  // Set the initial marker
+  this.initiateMarker();
+
+  // Add all layers
+  this.initialiseBaseLayers(options.baseLayers);
+  this.initialiseOverlays(options.overlays);
+};
+
+/**
+ * Initialise triggers
+ */
+CuriousMap.prototype.initialiseTriggers = function () {
+  var $this = this;
+
+  // SnapToLocation button trigger
+  if (undefined !== this.$snapCurrent) {
+    this.$snapCurrent.on('click', function () {
+      $this.snapToLocation();
+    });
+  }
+
+  // Dropping the marker somewhere on the map
+  this.$marker.on('dragend', function () {
+    $this.updateLocation(this.getLatLng());
+  });
+
+  // Enter in searchField
+  this.$searchField.on('keydown', function (e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      $this.searchAddress($(this).val());
+    }
+  });
+
+  // Search button pressed
+  this.$searchBtn.on('click', function () {
+    $this.searchAddress($this.$searchField.val());
+  });
+};
+
+/**
  * Update the marker and map location
- *
- * @param position
  */
 CuriousMap.prototype.updateLocation = function (position) {
+  // Set latitude and longitude from given position
   this.lat = position.lat;
   this.long = position.lng;
+
+  // Update all linked elements according to newly set coordinates
   this.$marker.setLatLng(position, { draggable: 'true' });
-  this.map.panTo(position);
+  this.$map.panTo(position);
   this.updateFormFields(position);
+  this.updateGeoJsonLayers();
 };
 
 /**
  * Perform a reverse lookup for the provided position
  * in order to retrieve address information and update
  * the related form fields.
- *
- * @param position
  */
 CuriousMap.prototype.updateFormFields = function (position) {
   var $this = this;
@@ -106,6 +188,42 @@ CuriousMap.prototype.updateFormFields = function (position) {
 };
 
 /**
+ * Update GeoJson-Layers with new data, according to the map's current boundaries
+ */
+CuriousMap.prototype.updateGeoJsonLayers = function () {
+  var $this = this;
+
+  var bbox = $this.$map.getBounds().toBBoxString();
+
+  // Generate url to fetch GeoJson objects
+  var generateUrl = function (settings) {
+    var parameters = Object.assign(
+      { bbox: bbox },
+      settings.parameters
+    );
+
+    return settings.url + L.Util.getParamString(parameters);
+  };
+
+  // Go through each GeoJsonLayer Object
+  $.each(this.geoJsonLayerObjects, function (index, geoJsonLayerObject) {
+    // Update the layer within the object with new GeoJson data, if any
+    var url = generateUrl(geoJsonLayerObject.settings);
+    if (url) {
+      $.ajax({
+        jsonp: true,
+        url: url,
+        dataType: 'json',
+        jsonpCallback: 'getJson',
+        success: function (data) {
+          geoJsonLayerObject.layer.addData(data);
+        }
+      });
+    }
+  });
+};
+
+/**
  * Clear all associated form fields
  */
 CuriousMap.prototype.clearFormFields = function () {
@@ -117,29 +235,99 @@ CuriousMap.prototype.clearFormFields = function () {
 };
 
 /**
- * Add a layer
+ * Add baseLayer according to given definition
  */
-CuriousMap.prototype.addLayer = function (settings) {
+CuriousMap.prototype.addBaseLayer = function (layerDefinition) {
   var layer;
 
-  if (settings.url === undefined) {
+  if (layerDefinition.url === undefined) {
     // Do not process layer without url
-  } else if (settings.type === 'TileLayer') {
-    // Create TileLayer
-    layer = this.createTileLayer(settings);
+  } else if (layerDefinition.type === 'TileLayer') {
+    layer = this.createTileLayer(layerDefinition);
   } else {
     // Do not process unknown layerType
   }
 
+  // Add layer to the map, if any
   if (layer !== undefined) {
-    this.map.addLayer(layer);
+    this.addBaseLayerToControl(layerDefinition.group, layer);
+    this.$map.addLayer(layer);
+  }
+
+  // Return the layer if any
+  return layer;
+};
+
+/**
+ * Add a layer to a group in the map control
+ */
+CuriousMap.prototype.addBaseLayerToControl = function (groupName, layer) {
+  var baseLayerLabel = '<span class="base-layer-label">%s</span>';
+
+  // Do not modify map control without groupName or layer to add.
+  if (groupName === undefined || layer === undefined) {
+    return;
+  }
+
+  // Update map control: Remove the modified group and then re-add
+  this.$mapControl.removeLayer(layer);
+  this.$mapControl.addBaseLayer(layer, baseLayerLabel.replace('%s', groupName));
+};
+
+CuriousMap.prototype.addOverlay = function (layerDefinition) {
+  var layer;
+
+  if (layerDefinition.url === undefined) {
+    // Do not process layer without url
+  } else if (layerDefinition.type === 'GeoJson' && layerDefinition.parameters !== undefined) {
+    layer = this.createGeoJsonLayer(layerDefinition);
+  } else if (layerDefinition.type === 'TileLayer') {
+    layer = this.createTileLayer(layerDefinition);
+  } else if (layerDefinition.type === 'WmsLayer') {
+    layer = this.createWmsLayer(layerDefinition);
+  } else {
+    // Do not process unknown layerType
+  }
+
+  // Add layer to the map, if any
+  if (layer !== undefined) {
+    // Enable snapping to this layer's features, if snapping was set to true in its definition
+    if (layerDefinition.snapping) {
+      this.enableSnappingForLayer(layer);
+    }
+    // Add the layer to mapControl and to the map
+    this.addOverlayToGroupInControl(layerDefinition.group, layer);
+    this.$map.addLayer(layer);
   }
 
   return layer;
 };
 
 /**
- * Create and return a TileLayer
+ * Add overlay layers to $mapControl grouped by groupName
+ */
+CuriousMap.prototype.addOverlayToGroupInControl = function (groupName, layer) {
+  var overlayLabel = '<span class="overlay-label">%s</span>';
+
+  // Do not modify map control without groupName or layer to add.
+  if (groupName === undefined || layer === undefined) {
+    return;
+  }
+
+  // Add layer to group according to its groupName
+  if (Object.prototype.hasOwnProperty.call(this.baseLayerGroups, groupName)) {
+    this.baseLayerGroups[groupName].addLayer(layer);
+  } else {
+    this.baseLayerGroups[groupName] = new L.LayerGroup([layer]);
+  }
+
+  // Add the layer in its group, after removing the old group if it exists
+  this.$mapControl.removeLayer(this.baseLayerGroups[groupName]);
+  this.$mapControl.addOverlay(this.baseLayerGroups[groupName], overlayLabel.replace('%s', groupName));
+};
+
+/**
+ * Create and return a Tile-Layer
  */
 CuriousMap.prototype.createTileLayer = function (settings) {
   return new L.TileLayer(
@@ -154,107 +342,121 @@ CuriousMap.prototype.createTileLayer = function (settings) {
 };
 
 /**
- * Initialise the baseLayer
+ * Create and return a WMS-Layer
  */
-CuriousMap.prototype.initialiseBaseLayer = function (settings) {
-  var $this = this;
-
-  if (settings.url === undefined || settings.type === undefined) {
-    $(document).ready(function () {
-      $this.$modal
-        .modal()
-        .attr('class', 'modal fade')
-        .addClass('default')
-        .find('.alert_no_base_layer')
-        .show();
-    });
-  } else {
-    this.addLayer(settings);
-  }
-};
-
-/**
- * Initialise Overlays
- */
-CuriousMap.prototype.initialiseOverlays = function (overlays) {
-  var $this = this;
-
-  // Add each overlay to the map
-  $.each(overlays, function (name, overlay) {
-    L.WMS.overlay(
-      overlay.url,
-      {
-        layers: overlay.layers.join(),
-        format: overlay.format || 'image/png',
-        transparent: overlay.transparent || true
-      }
-    ).addTo($this.map);
-  });
-};
-
-/**
- * Initialise search fields
- */
-CuriousMap.prototype.initialiseSearchFields = function () {
-  this.$snapCurrent = $(`#${this.formId}_location_snap`);
-  this.$searchField = $(`#${this.formId}_location_search_input`);
-  this.$searchBtn = $(`#${this.formId}_location_search_btn`);
-};
-
-/**
- * Link the form fields to this object
- */
-CuriousMap.prototype.initialiseFormFields = function (fields) {
-  var $this = this;
-
-  $this.fields = {};
-  $.each(fields, function (name, options) {
-    if (options.name === undefined) {
-      options.name = name;
+CuriousMap.prototype.createWmsLayer = function (settings) {
+  return L.WMS.overlay(
+    settings.url,
+    {
+      layers: settings.layers.join(),
+      format: settings.format || 'image/png',
+      transparent: settings.transparent || true
     }
-
-    options.$field = $(`#${$this.formId}_${options.name}`);
-    $this.fields[name] = options;
-  });
+  );
 };
 
 /**
- * Initialise triggers
+ * Create and return a GeoJsonLayer, and register it for updating on location change
  */
-CuriousMap.prototype.initialiseTriggers = function () {
+CuriousMap.prototype.createGeoJsonLayer = function (settings) {
   var $this = this;
 
-  // SnapToLocation button trigger
-  if (undefined !== this.$snapCurrent) {
-    this.$snapCurrent.on('click', function () {
-      $this.snapToLocation();
-    });
-  }
+  // Set style for circleMarkers if it has not been set
+  this.geoJsonMarkerStyle = this.geoJsonMarkerStyle || {
+    radius: 3,
+    fillColor: '#ff7800',
+    color: '#000',
+    weight: 0.5,
+    opacity: 1,
+    fillOpacity: 0.8
+  };
 
-  // Dropping the marker somewhere on the map
-  this.$marker.on('dragend', function () {
-    $this.updateLocation(this.getLatLng());
-  });
-
-  // Enter in searchField
-  this.$searchField.on('keydown', function (e) {
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      $this.searchAddress($(this).val());
+  // Create an empty geoJson layer
+  var layer = L.geoJson(null, {
+    pointToLayer: function (feature, location) {
+      return L
+        .circleMarker(location, $this.geoJsonMarkerStyle)
+        .bindPopup(JSON.stringify(feature, location));
     }
   });
 
-  // Search button pressed
-  this.$searchBtn.on('click', function () {
-    $this.searchAddress($this.$searchField.val());
+  // Add this layer and its metadata to the geoJsonLayerObjects list
+  this.geoJsonLayerObjects.push({
+    layer: layer,
+    settings: settings
   });
+
+  // Return the geoJsonLayer
+  return layer;
+};
+
+/**
+ * Initialise the baseLayers, according to given layerDefinitions
+ */
+CuriousMap.prototype.initialiseBaseLayers = function (layerDefinitions) {
+  var $this = this;
+
+  this.baseLayerGroups = {};
+
+  // Go through each layerDefinition
+  $.each(layerDefinitions, function (index, layerDefinition) {
+    if (layerDefinition.url === undefined || layerDefinition.type === undefined) {
+      // Warn the user when a base layer cannot be initialised
+      $(document).ready(function () {
+        $this.$modal
+          .modal()
+          .attr('class', 'modal fade')
+          .addClass('default')
+          .find('.alert_no_base_layer')
+          .show();
+      });
+    } else {
+      // Add the layer according to its definition
+      $this.addBaseLayer(layerDefinition);
+    }
+  });
+};
+
+/**
+ * Initialise Overlays, according to given layerDefinitions
+ */
+CuriousMap.prototype.initialiseOverlays = function (layerDefinitions) {
+  var $this = this;
+
+  this.overlayGroups = {};
+
+  $.each(layerDefinitions, function (index, layerDefinition) {
+    if (layerDefinition.url && layerDefinition.type) {
+      $this.addOverlay(layerDefinition);
+    }
+  });
+};
+
+/**
+ * Add a MapControl to the map (uses this.$mapControl by default)
+ */
+CuriousMap.prototype.addMapControl = function (control) {
+  var mapControl = control || this.$mapControl;
+
+  this.$mapControl = mapControl
+    .layers()
+    .addTo(this.$map);
 };
 
 /**
  * Update the marker according to current latitude and longitude
  */
-CuriousMap.prototype.updateMarker = function () {
-  this.$marker = L.marker([this.lat, this.long], { draggable: 'true' }).addTo(this.map);
+CuriousMap.prototype.initiateMarker = function () {
+  this.$marker = L.marker([this.lat, this.long], { draggable: 'true' }).addTo(this.$map);
+  this.$marker.snapediting = new L.Handler.MarkerSnap(this.$map, this.$marker);
+};
+
+/**
+ * Enable snapping for given layer
+ */
+CuriousMap.prototype.enableSnappingForLayer = function (layer) {
+  this.$marker.snapediting.addGuideLayer(layer);
+  this.$marker.snapediting.enable();
 };
 
 /**
@@ -263,18 +465,19 @@ CuriousMap.prototype.updateMarker = function () {
 CuriousMap.prototype.searchAddress = function (address) {
   var $this = this;
 
+  var position;
+
   $.getJSON('https://nominatim.openstreetmap.org/search?format=json&limit=1&q=' + address, function (data) {
     if (data.length) {
-      var details = data[0];
-      var position = new L.LatLng(details.lat, details.lon);
+      position = new L.LatLng(data[0].lat, data[0].lon);
       $this.updateLocation(position);
-      $this.map.setZoom($this.determineZoomLevel(details.type));
+      $this.$map.setZoom($this.determineZoomLevel(data[0].type));
     }
   });
 };
 
 /**
- * Determine appropriate zoom-level based on information type
+ * Determine appropriate zoom-level based on information level-type (e.g. 'city') of a JSON response
  */
 CuriousMap.prototype.determineZoomLevel = function (type) {
   var level;
@@ -293,24 +496,28 @@ CuriousMap.prototype.determineZoomLevel = function (type) {
 };
 
 /**
- * Focus the searchField when focusing this object
+ * Focus CuriousMap by focusing it's searchField, if any
  */
 CuriousMap.prototype.focus = function () {
-  this.$searchField.focus();
+  if (this.$searchField) this.$searchField.focus();
 };
 
 /**
  * Snap to current location (HTTPS/Local only)
  */
 CuriousMap.prototype.snapToLocation = function () {
-  if (window.location.protocol === 'https:') {
+  if (
+    window.location.protocol === 'https:' ||
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '127.0.0.1'
+  ) {
     // Locate device's location on the map
-    this.map.locate({
+    this.$map.locate({
       watch: true,
       enableHighAccuracy: true
     });
   } else {
-    // Show a warning that this functionality does not work over http
+    // Show a warning that this functionality does not work over http, unless origin is localhost
     this.$modal
       .modal()
       .attr('class', 'modal fade')
